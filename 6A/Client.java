8768559476fff0ff620937d3ea07ca1e868fc2bc
NikhilKalloli <1ms22cs091@msrit.edu>
2024-07-04 00:08:28 +0530

    Add BellmanFord

--- 6A/Client.java
+++ 6A/Client.java
@@ -4,13 +4,17 @@ import java.io.*;
 
 public class Client {
     public static void main(String[] args) throws Exception {
+
         Socket sock = new Socket("127.0.0.1", 4000);
         System.out.println("Enter the filename");
+
         BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
         String fname = keyRead.readLine();
+
         OutputStream ostream = sock.getOutputStream();
         PrintWriter pwrite = new PrintWriter(ostream, true);
         pwrite.println(fname);
+        
         InputStream istream = sock.getInputStream();
         BufferedReader socketRead = new BufferedReader(new InputStreamReader(istream));
         String str;
--- 6A/Server.java
+++ 6A/Server.java
@@ -3,13 +3,17 @@ import java.io.*;
 
 public class Server {
     public static void main(String[] args) throws Exception {
+
         ServerSocket sersock = new ServerSocket(4000);
         System.out.println("Server ready for connection");
         Socket sock = sersock.accept();
         System.out.println("Connection Is successful and waiting for chatting");
+
         InputStream istream = sock.getInputStream();
         BufferedReader fileRead = new BufferedReader(new InputStreamReader(istream));
         String fname = fileRead.readLine();
+
+        
         BufferedReader ContentRead = new BufferedReader(new FileReader(fname));
         OutputStream ostream = sock.getOutputStream();
         PrintWriter pwrite = new PrintWriter(ostream, true);
