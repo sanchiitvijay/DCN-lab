686dcc4424f665f375169260e1d411792541dfa8
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-11 01:16:15 +0530

    Update BellmanFord.java

--- 4A/BellmanFord.java
+++ 4A/BellmanFord.java
@@ -1,51 +1,62 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class BellmanFord {
-    static int[] bellmanFord(int V, ArrayList<ArrayList<Integer>> edges, int S){
-        int dist[] = new int[V];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[S] = 0;
-
-        for(int i = 0; i < V-1; i++){
-            for(ArrayList<Integer> it: edges){
-                int u = it.get(0);
-                int v = it.get(1);
-                int wt = it.get(2);
-
-                if(dist[u] + wt < dist[v]){
-                    dist[v] = dist[u] + wt;
-                }
-            }
-        }
-
-        for(ArrayList<Integer> it : edges){
-            int u = it.get(0);
-            int v = it.get(1);
-            int wt = it.get(2);
-
-            if(dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]){
-                int temp[] = new int[1];
-                temp[0] = -1;
-                return temp;
-            }
-        }
-
-        return dist;
-    }
-    public static void main(String[] args) {
-        int V = 5;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-
-        adj.add(new ArrayList<Integer>(Arrays.asList(0, 1, 5)));
-        adj.add(new ArrayList<Integer>(Arrays.asList(1, 2, 20)));
-        adj.add(new ArrayList<Integer>(Arrays.asList(1, 3, 10)));
-        adj.add(new ArrayList<Integer>(Arrays.asList(3, 4, 5)));
-        adj.add(new ArrayList<Integer>(Arrays.asList(2, 4, 1)));
-
-        int source = 0;
-        int dist[] = bellmanFord(V, adj, source);
-
-        System.out.println(Arrays.toString(dist));
+public class BellmanFord{
+    public static final int MAX = Integer.MAX_VALUE;
+    
+    public static void bellmanFordEvaluation(int source, int N, int edges[][]){
+        int dist[] = new int[N];
+        for(int it = 0; it < N; ++it) dist[it] = MAX;
+        dist[source] = 0;
         
+        int u,v,wt;
+        
+        //N-1 times
+        for(int it = 1; it < N; ++it){
+            for(int e = 0; e < edges.length; ++e){
+                u = edges[e][0];
+                v = edges[e][1];
+                wt = edges[e][2];
+                if( dist[u] + wt < dist[v] ) 
+                    dist[v] = dist[u] + wt;
+            }
+        }
+        
+        //Nth time
+        for(int e = 0;e < edges.length; ++e){
+            u = edges[e][0];
+            v = edges[e][1];
+            wt = edges[e][2];
+            if( dist[u] + wt < dist[v] ){
+                System.out.println("Negative cycle exists in the graph!!");
+                return;
+            }
+        }
+        
+        for(int node = 0; node < N; ++node){
+            System.out.println("Distance from "+source+" to "+node+" : "+dist[node]);
+        }
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter no. of vertices : ");
+        int N = sc.nextInt();
+        
+        System.out.print("Enter no. of edges : ");
+        int E = sc.nextInt();
+        
+        int edges[][] = new int[E][3];
+        for(int i=0;i<E;++i){
+            System.out.print("Edge-"+(i+1)+" : ");
+            edges[i][0] = sc.nextInt();
+            edges[i][1] = sc.nextInt();
+            edges[i][2] = sc.nextInt();
+        }
+        
+        System.out.print("Enter source node : ");
+        int source = sc.nextInt();
+        
+        bellmanFordEvaluation(source, N, edges);
     }
 }
